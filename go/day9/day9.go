be25a2067a9d761a15a3fa89f8c774e8ad84cb7b
Mike Conroy <mike.conroy@salesforce.com>
2024-12-11 15:38:43 +0000

    Day9

--- go/day9/day9.go
+++ go/day9/day9.go
@@ -29,7 +29,7 @@ func (list *DoublyLinkedList) append(node *Node) {
 
 func (list *DoublyLinkedList) print() {
 	node := list.head
-	for node.next != nil {
+	for node != nil {
 		fmt.Println("Node ID:", node.id, "Size:", node.size)
 		node = node.next
 	}
@@ -44,25 +44,130 @@ type Node struct {
 
 func generateDll(diskMap string) *DoublyLinkedList {
 	list := &DoublyLinkedList{}
-	id := 0
 	for block := range diskMap {
 		size := int(diskMap[block] - '0')
+		var id int
+		if block%2 == 0 {
+			id += block / 2
+		} else {
+			id = -1
+		}
 		node := &Node{
 			id:   id,
 			size: size,
 		}
-		id += size
+
 		list.append(node)
 	}
 	return list
 }
 
+func rearrangeWholeFiles(list *DoublyLinkedList) {
+	last := list.tail
+	for last != nil {
+		for last.id == -1 {
+			last = last.prev
+		}
+		space := nextFreeBlock(list.head, last)
+
+		for space != last {
+			space = nextFreeBlock(space, last)
+			if space.size >= last.size {
+				break
+			}
+			space = space.next
+		}
+		if space.id == -1 && space != last {
+			if space.size == last.size {
+				space.id = last.id
+				last.id = -1
+			} else {
+				newSpace := &Node{
+					id:   -1,
+					size: space.size - last.size,
+					prev: space,
+					next: space.next,
+				}
+				space.size = last.size
+				space.id = last.id
+				last.id = -1
+				space.next = newSpace
+				newSpace.next.prev = newSpace
+			}
+		}
+		last = last.prev
+	}
+}
+
+func rearrange(list *DoublyLinkedList) {
+	last := list.tail
+	space := nextFreeBlock(list.head, list.tail)
+	for {
+		if last.id == -1 {
+			last = last.prev
+		}
+		if last.size > space.size {
+			space.id = last.id
+			last.size -= space.size
+		} else if last.size < space.size {
+			newSpace := &Node{id: -1, size: space.size - last.size, prev: space, next: space.next}
+			space.next.prev = newSpace
+			space.id = last.id
+			space.size = last.size
+			space.next = newSpace
+			last.id = -1
+			last = last.prev
+		} else {
+			space.id = last.id
+			last.id = -1
+			last = last.prev
+		}
+		space = nextFreeBlock(space, last)
+
+		if space.id != -1 || space == last {
+			break
+		}
+	}
+
+}
+
+func nextFreeBlock(from *Node, upTo *Node) *Node {
+	node := from
+	for node.id != -1 {
+		if node == upTo {
+			return node
+		}
+		node = node.next
+	}
+	return node
+}
+
+func calculateChecksum(list *DoublyLinkedList) int {
+	checksum := 0
+	pos := 0
+	node := list.head
+	for node != nil {
+		if node.id != -1 {
+			for i := pos; i < pos+node.size; i++ {
+				checksum += (i * node.id)
+			}
+		}
+		pos += node.size
+		node = node.next
+	}
+	return checksum
+}
+
 func part1(input []string) string {
 	list := generateDll(input[0])
-	list.print()
-	return fmt.Sprint(1)
+	rearrange(list)
+	result := calculateChecksum(list)
+	return fmt.Sprint(result)
 }
 
 func part2(input []string) string {
-	return fmt.Sprint(2)
+	list := generateDll(input[0])
+	rearrangeWholeFiles(list)
+	result := calculateChecksum(list)
+	return fmt.Sprint(result)
 }
--- go/day9/day9_test.go
+++ go/day9/day9_test.go
@@ -7,16 +7,39 @@ import (
 )
 
 func TestDay9Part1(t *testing.T) {
+	expect := "60"
+	result := part1([]string{"12345"})
+	if result != expect {
+		t.Fatal("Day 9 - Part 1 12345 output should be", expect, "but got", result)
+	}
 	input := utils.ReadFileIntoSlice("input_test")
-	expect := "1928"
-	if part1(input) != expect {
-		t.Fatal("Day 9 - Part 1 output should be " + expect)
+	expect = "1928"
+	result = part1(input)
+	if result != expect {
+		t.Fatal("Day 9 - Part 1 output should be " + expect + "but got" + result)
+	}
+}
+
+func TestChecksum(t *testing.T) {
+	expect := 10
+	list := &DoublyLinkedList{}
+	list.append(&Node{
+		id:   0,
+		size: 2,
+	})
+	list.append(&Node{
+		id:   2,
+		size: 2,
+	})
+	result := calculateChecksum(list)
+	if result != expect {
+		t.Fatal("Day 9 - Checksum should be", expect, "but was", result)
 	}
 }
 
 func TestDay9Part2(t *testing.T) {
 	input := utils.ReadFileIntoSlice("input_test")
-	if part2(input) != "2" {
-		t.Fatal("Day 9 - Part 2 output should be 2")
+	if part2(input) != "2858" {
+		t.Fatal("Day 9 - Part 2 output should be 2858 but got", part2(input))
 	}
 }
