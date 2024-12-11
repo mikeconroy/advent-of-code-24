44b854b25b682d5cfde7e6bc658932e9bf0c4a70
Mike Conroy <mike.conroy@salesforce.com>
2024-12-11 11:07:51 +0000

    Implement Linked List for Day9

--- go/day9/day9.go
+++ go/day9/day9.go
@@ -11,8 +11,55 @@ func Run() (string, string) {
 	return part1(input), part2(input)
 }
 
-func part1(input []string) string {
+type DoublyLinkedList struct {
+	head *Node
+	tail *Node
+}
 
+func (list *DoublyLinkedList) append(node *Node) {
+	if list.head == nil {
+		list.head = node
+	}
+	if list.tail != nil {
+		list.tail.next = node
+		node.prev = list.tail
+	}
+	list.tail = node
+}
+
+func (list *DoublyLinkedList) print() {
+	node := list.head
+	for node.next != nil {
+		fmt.Println("Node ID:", node.id, "Size:", node.size)
+		node = node.next
+	}
+}
+
+type Node struct {
+	id   int
+	size int
+	next *Node
+	prev *Node
+}
+
+func generateDll(diskMap string) *DoublyLinkedList {
+	list := &DoublyLinkedList{}
+	id := 0
+	for block := range diskMap {
+		size := int(diskMap[block] - '0')
+		node := &Node{
+			id:   id,
+			size: size,
+		}
+		id += size
+		list.append(node)
+	}
+	return list
+}
+
+func part1(input []string) string {
+	list := generateDll(input[0])
+	list.print()
 	return fmt.Sprint(1)
 }
 
