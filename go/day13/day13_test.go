f57e08c0d3f840cbf940cb74f09f19e6d91ec3ca
Mike Conroy <mikecon@outlook.com>
2024-12-15 18:20:16 +0000

    Fix D13P2 Test

--- go/day13/day13_test.go
+++ go/day13/day13_test.go
@@ -17,7 +17,7 @@ func TestDay13Part1(t *testing.T) {
 
 func TestDay13Part2(t *testing.T) {
 	input := utils.ReadFileIntoSlice("input_test")
-	expect := "2"
+	expect := "875318608908"
 	result := part2(input)
 	if result != expect {
 		t.Fatal("Day 13 - Part 2 output should be", expect, "but got", result)
